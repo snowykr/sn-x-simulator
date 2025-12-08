96a6a38f44b7f751bdf5c9e63daec924d2f684f5
Jun, Koo <snowykr22@gmail.com>
2025-12-08 23:43:59 +0900

    feat: Display uninitialized registers as * in trace output

--- snx/simulator.py
+++ snx/simulator.py
@@ -17,6 +17,8 @@ class SNXSimulator:
     ):
         self.regs: list[int] = [0] * reg_count
         self.memory: list[int] = [0] * mem_size
+        self._reg_initialized: list[bool] = [False] * reg_count
+        self._mem_initialized: list[bool] = [False] * mem_size
         self.pc: int = 0
         self.running: bool = True
 
@@ -78,28 +80,33 @@ class SNXSimulator:
             dest = self._get_reg_idx(args[0])
             addr = self._calc_effective_addr(args[1])
             self.regs[dest] = addr
+            self._reg_initialized[dest] = True
 
         elif op == "LD":
             dest = self._get_reg_idx(args[0])
             addr = self._calc_effective_addr(args[1])
             self.regs[dest] = self.memory[addr]
+            self._reg_initialized[dest] = True
 
         elif op == "ST":
             src = self._get_reg_idx(args[0])
             addr = self._calc_effective_addr(args[1])
             self.memory[addr] = self.regs[src]
+            self._mem_initialized[addr] = True
 
         elif op == "ADD":
             dest = self._get_reg_idx(args[0])
             val1 = self.regs[self._get_reg_idx(args[1])]
             val2 = self.regs[self._get_reg_idx(args[2])]
             self.regs[dest] = val1 + val2
+            self._reg_initialized[dest] = True
 
         elif op == "SLT":
             dest = self._get_reg_idx(args[0])
             val1 = self.regs[self._get_reg_idx(args[1])]
             val2 = self.regs[self._get_reg_idx(args[2])]
             self.regs[dest] = 1 if val1 < val2 else 0
+            self._reg_initialized[dest] = True
 
         elif op == "BZ":
             cond_reg = self._get_reg_idx(args[0])
@@ -119,6 +126,7 @@ class SNXSimulator:
                 target_pc = self._calc_effective_addr(args[1])
 
             self.regs[link_reg] = next_pc
+            self._reg_initialized[link_reg] = True
             self.pc = target_pc
 
         elif op == "HLT":
@@ -126,6 +134,12 @@ class SNXSimulator:
 
         else:
             raise ValueError(f"Unknown opcode: {op}")
+
+    def get_reg_init_flags(self) -> tuple[bool, ...]:
+        return tuple(self._reg_initialized)
+
+    def get_memory_init_flags(self) -> tuple[bool, ...]:
+        return tuple(self._mem_initialized)
 
     def run(self, max_steps: int | None = None) -> None:
         steps = 0
